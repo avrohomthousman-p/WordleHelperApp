64fbd393f5df8ba37f60cab21458dccc5281e434
avrohomthousman <ahousman@student.touro.edu>
2022-12-08 16:54:33 -0500

    Created new empty activity

---
+++ app/src/main/java/com/example/wordle_helper/Activities/WordDisplayActivity.java
@@ -0,0 +1,16 @@
+package com.example.wordle_helper.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.wordle_helper.R;
+
+public class WordDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_display);
+    }
+}
