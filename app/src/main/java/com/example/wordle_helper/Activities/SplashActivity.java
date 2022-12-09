f80fbefe1cc3cb5d98cb7b2ee5518d63324bb86b
avrohomthousman <ahousman@student.touro.edu>
2022-12-09 15:05:40 -0500

    Added splash activity and launcher icon

---
+++ app/src/main/java/com/example/wordle_helper/Activities/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.example.wordle_helper.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity (new Intent(getApplicationContext (),
+                MainActivity.class));
+
+        finish ();
+    }
+}
